34390ca1c551f88f6a6bc0b28bcee327636d15a0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-28 18:10:21 +0200

    Created new layout of default page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-    <VApp>
-        <NuxtPage />
-    </VApp>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Billing System</v-app-bar-title>
+        </v-app-bar>
+
+        <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
+            <!--  -->
+        </v-navigation-drawer>
+
+        <v-main>
+            <div class="pa-8">
+                <NuxtPage />
+            </div>       
+        </v-main>
+    </v-app>
 </template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay();
+
+const drawer = ref(false)
+</script>

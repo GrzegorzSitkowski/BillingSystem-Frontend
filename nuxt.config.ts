135ca2c8d5838a8a550b5319ead629407e71794a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-28 11:29:01 +0200

    Installed package of pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,18 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
+
+  imports:{
+    dirs: ['stores'],
+  },
+  
   modules: [
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         config.plugins?.push(vuetify({autoImport: true}))
       })
     },
+    '@pinia/nuxt'
   ],
   vite:{
     vue: {

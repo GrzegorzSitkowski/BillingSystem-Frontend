54d1dcbb263c95f7e286b34e7ca6ade967e36490
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-29 17:10:07 +0200

    Added colors of application

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
     <v-app>
-        <v-app-bar>
+        <v-app-bar color="brand">
             <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
             <v-app-bar-title>Billing System</v-app-bar-title>
         </v-app-bar>
 
-        <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
+        <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer" color="brand">
             <VList>
                 <VListItem v-for="item in menuItems" :key="item.name" :title="item.name" :prepend-icon="item.icon" :to="item.url"></VListItem>
             </VList>

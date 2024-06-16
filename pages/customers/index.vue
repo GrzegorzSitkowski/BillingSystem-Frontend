6e02243b5ecf5b8226963f35ee2166b23dde43d0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-06-16 17:55:04 +0200

    Added page to list of customers

---
+++ pages/customers/index.vue
@@ -0,0 +1,73 @@
+<template>
+    <VCard>
+        <v-toolbar color="transparent">
+            <v-toolbar-title>
+                List of Customers
+            </v-toolbar-title>
+
+            <template v-slot:append>
+                <div class="ml-2">
+                    <v-btn color="primary" variant="flat" prepend-icon="mdi-plus">Add</v-btn>
+                </div>
+            </template>
+        </v-toolbar>
+
+		<v-data-table :loading="loading" :items="items" :headers="headers" items-per-page-text="Rows"
+            :items-per-page-options="[10, 20, 50]" page-text="{0}-{1} of {2}" no-data-text="Not added any customers" loading-text="Loading">
+
+            <template v-slot:item.url="{ value }">
+                <a :href="value" target="_blank">{{ value }}</a>
+            </template>
+
+            <template v-slot:item.createDate="{ value }">
+                {{ dayjs(value).format('DD.MM.YYYY HH:mm') }}
+            </template>
+
+            <template v-slot:item.action="{ item }">
+                <div class="text-no-wrap">
+                    <VBtn icon="mdi-delete" title="Delete" variant="flat"></VBtn>
+                    <VBtn icon="mdi-pencil" title="Edit" variant="flat"></VBtn>
+                </div>
+            </template>
+        </v-data-table>
+    </VCard>
+</template>
+
+<script setup>
+
+const dayjs = useDayjs();
+const loading = ref(false);
+const items = ref([]);
+
+const headers = ref([
+    {title: 'Id', value: 'id'},
+    {title: 'Full Name', value: 'fullName'},
+    {title: 'City', value: 'city'},
+    {title: 'Email', value: 'email'},
+    {title: 'Pay rate', value: 'payRate'},
+    {title: 'Balance', value: 'balance'},
+    {title: '', value: 'action', align: 'end'},
+]);
+
+const loadData = () => {
+    loading.value = true;
+
+    useWebApiFetch('/Customer/List')
+        .then(({ data, error }) => {
+            if (data.value) {
+                items.value = data.value.customers;
+            } else if (error.value) {
+                items.value = [];
+            }
+        })
+        .finally(() => {
+            loading.value = false;
+        });
+};
+
+onMounted(() => {
+    loadData();
+});
+
+
+</script>

d71e74ed09b8601725cba8ed137586e41186bb7e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-07-16 17:53:51 +0200

    Created index of Billing Dashboard

---
+++ pages/billing/index.vue
@@ -0,0 +1,75 @@
+<template>
+    <VCard>
+        <v-toolbar color="transparent">
+            <v-toolbar-title>
+                Billing Dashboard
+            </v-toolbar-title>
+        
+            <template v-slot:append>
+                <div class="ml-2">
+                    <v-btn color="primary" variant="flat" prepend-icon="mdi-plus" to="/readings/add">Add</v-btn>
+                </div>
+            </template>
+        </v-toolbar>
+
+		<v-data-table :loading="loading" :items="items" :headers="headers" items-per-page-text="Rows"
+            :items-per-page-options="[10, 20, 50]" page-text="{0}-{1} of {2}" no-data-text="Not added any readings" loading-text="Loading">
+
+            <template v-slot:item.url="{ value }">
+                <a :href="value" target="_blank">{{ value }}</a>
+            </template>
+
+            <template v-slot:item.createDate="{ value }">
+                {{ dayjs(value).format('DD.MM.YYYY HH:mm') }}
+            </template>
+
+            <template v-slot:item.action="{ item }">
+                <div class="text-no-wrap">
+                    <!-- <VBtn icon="mdi-magnify-plus" title="Details" variant="flat" :to="`/readings/details/${item.id}`"></VBtn> -->
+                    <!-- <VBtn icon="mdi-pencil" title="Edit" variant="flat" :to="`/readings/${item.id}`"></VBtn> -->
+                    <!-- <VBtn icon="mdi-delete" title="Delete" variant="flat" :loading="item.deleting" @click="deleteReading(item)"></VBtn> -->
+                </div>
+            </template>
+        </v-data-table>
+        <ConfirmDialog ref="confirmDialog" />
+    </VCard>
+</template>
+
+<script setup>
+const globalMessageStore = useGlobalMessageStore();
+const { getErrorMessage } = useWebApiResponseParser();
+const dayjs = useDayjs();
+const loading = ref(false);
+const items = ref([]);
+
+const confirmDialog = ref(null);
+
+const headers = ref([
+    {title: 'Customer Name', value: 'customerName'},
+    {title: 'Customer Id', value: 'customerId'},
+    {title: 'Lessons', value: 'lessons'},
+    {title: 'Price', value: 'price'},
+    {title: '', value: 'action', align: 'end'},
+]);
+
+const loadData = () => {
+    loading.value = true;
+
+    useWebApiFetch('/Readings/List')
+        .then(({ data, error }) => {
+            if (data.value) {
+                items.value = data.value.readings;
+            } else if (error.value) {
+                items.value = [];
+            }
+        })
+        .finally(() => {
+            loading.value = false;
+        });
+};
+
+onMounted(() => {
+    loadData();
+});
+
+</script>

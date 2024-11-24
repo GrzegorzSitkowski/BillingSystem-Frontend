8d6e376e85cbe55453a3b297dad213427b8beff6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-11-24 21:20:27 +0100

    Added shortcut on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Billing system to invoice customers in your language school.</div>
+    <div class="d-flex flex-wrap ga-3">
+        <v-col cols="12" sm="6">
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/customers'">Customers</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/readings'">Readings</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/billing/readings'">Billing Dashboard</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/payments'">Payments</VBtn>
+        </v-col>
+        <v-col cols="12" sm="6">         
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/RecordOfInvoices'">Invoices</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/RecordOfInvoices/Correction'">Corrections</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/billing/interest'">Interest to bill</VBtn>
+            <VBtn height="150" min-width="150" style="margin: 5px;" :to="'/RecordOfInterest'">Interest notes</VBtn>
+        </v-col>
+</div>
 </template>
+
+<script setup>
+import { VBtn } from 'vuetify/components';
+</script>
+
